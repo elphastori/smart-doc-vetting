394a5c8cab2975f8c538277092576f5d0bee1a47
Brandon <brandon2255p@gmail.com>
2019-11-12 22:46:44 +0200

    Added id doc test

--- keywords/confience.py
+++ keywords/confience.py
@@ -2,7 +2,7 @@ from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 
 space = ' '
-employment_keywords = [
+_EMPLOYMENT_KEYWORDS = [
     'salary',
     'advice',
     'payslip',
@@ -12,11 +12,21 @@ employment_keywords = [
     'hours',
     'employee',
     'earnings']
-    
+_ID_KEYWORDS = [
+    'rsa',
+    'country',
+    'id',
+    'home affairs']
 def calculate_confidence_employment(input_list : list):
+    return _calc_confidence(_EMPLOYMENT_KEYWORDS, input_list)
+
+def calculate_confidence_id(input_list : list):
+    return _calc_confidence(_ID_KEYWORDS, input_list)
+
+def _calc_confidence(key_words_list : list, input_list: list):
     words = space.join(input_list)
     final_ratio = 0
-    for keyword in employment_keywords:
+    for keyword in key_words_list:
         ratio = fuzz.partial_ratio(words, keyword)
         if ratio > 50:
             final_ratio += 1
