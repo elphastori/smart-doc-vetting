7887a5d19c631aa6c916bca8920b709f1fabc9ac
Elphas <elphastori@gmail.com>
2019-11-12 21:52:48 +0000

    Extract web entities from Vision API

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import io
 import os
+import json
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -9,24 +10,61 @@ load_dotenv()
 from google.cloud import vision
 from google.cloud.vision import types
 
-# Instantiates a client
-client = vision.ImageAnnotatorClient()
+def annotate():
+    # Instantiates a client
+    client = vision.ImageAnnotatorClient()
 
-# The name of the image file to annotate
-file_name = os.path.abspath('IdDocument-2019-11-12-17.21.png.Png')
+    # The name of the image file to annotate
+    file_name = os.path.abspath('IdDocument-2019-11-12-17.21.png.Png')
 
-# Loads the image into memory
-with io.open(file_name, 'rb') as image_file:
-    content = image_file.read()
+    # Loads the image into memory
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
 
-image = types.Image(content=content)
+    image = types.Image(content=content)
 
-# Performs label detection on the image file
-response = client.label_detection(image=image)
-labels = response.label_annotations
+    # Performs label detection on the image file
+    response = client.annotate_image({
+    'image': image,
+    'features': [
+        {'type': vision.enums.Feature.Type.TEXT_DETECTION},
+        {'type': vision.enums.Feature.Type.WEB_DETECTION}
+        ],
+    })
 
-print("Smart Document Verification\n")
+    return response
 
-print('Labels:')
-for label in labels:
-    print(label.description)
+def report(annotations):
+    """Prints detected features in the provided web annotations."""
+    if annotations.pages_with_matching_images:
+        print('\n{} Pages with matching images retrieved'.format(
+            len(annotations.pages_with_matching_images)))
+
+        for page in annotations.pages_with_matching_images:
+            print('Url   : {}'.format(page.url))
+
+    if annotations.full_matching_images:
+        print('\n{} Full Matches found: '.format(
+              len(annotations.full_matching_images)))
+
+        for image in annotations.full_matching_images:
+            print('Url  : {}'.format(image.url))
+
+    if annotations.partial_matching_images:
+        print('\n{} Partial Matches found: '.format(
+              len(annotations.partial_matching_images)))
+
+        for image in annotations.partial_matching_images:
+            print('Url  : {}'.format(image.url))
+
+    if annotations.web_entities:
+        print('\n{} Web entities found: '.format(
+              len(annotations.web_entities)))
+
+        for entity in annotations.web_entities:
+            print('Score      : {}'.format(entity.score))
+            print('Description: {}'.format(entity.description))
+
+if __name__ == '__main__':
+    print("Smart Document Verification\n")
+    report(annotate().web_detection)
