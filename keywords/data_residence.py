6b84f8c283b2473406ab53d71266705bdecc33c7
Elphas <elphastori@gmail.com>
2019-11-16 21:49:23 +0000

    Added proof of residence data

--- keywords/data_residence.py
+++ keywords/data_residence.py
@@ -2,17 +2,18 @@ from models import Vision
 from models import WebEntity
 
 web_entities = [
-    WebEntity("Screenshot", 0.3131629228591919),
-    WebEntity("Product design", 0.26272037625312805),
-    WebEntity("Bank", 0.243599995970726),
-    WebEntity("Design", 0.2150000035762787),
-    WebEntity("Product", 0.21230000257492065),
-    WebEntity("Line", 0.21158140897750854),
-    WebEntity("", 0.19609999656677246),
-    WebEntity("F.N.B. Corporation", 0.19609999656677246),
-    WebEntity("Meter", 0.17790000140666962)
+    WebEntity("", 0.39079999923706055,
+    WebEntity("8ta", 0.28630000352859497),
+    WebEntity("Telkom", 0.2842000126838684),
+    WebEntity("Organization", 0.213968887925148}),
+    WebEntity("Web page", 0.20840270817279816),
+    WebEntity("Mobile phone", 0.18060000240802765),
+    WebEntity("Contract", 0.17749999463558197),
+    WebEntity("Product", 0.14560000598430634),
+    WebEntity("Invoice", 0.14090000092983246),
+    WebEntity("Product design", 0.13640211522579193),
 ]
 
-text = "05/11/2019\nFNB Verified Statement\nCarlswald\nReference Number:\nVODSWRV4974F\nPostnet Suite 94,P rivate Bag X121\nHalfway House ,1685\n250117\nTo verify this statement, please keep the above reference number and the client's\nID number/business account number on hand. Visit www.fnb.co.za, select Contact\nus Tools on the menu, followed by Verify Statement and follow the on-screen\nFNB\nBranch Code\ninstructions. The reference number is valid for a minimum of 3 months.\nCustomer VAT Registration Number Not Provided\nBank VAT Registration Number 4210102051\nFirst National Bank\nhow can we help you?\nCopy Tax Invoice/S tatement Number 113\nStatement Period : 20 September 2019 to 19 October 2019\nStatement Date 19 October 2019\nBBST113\n015111\nMISS TIRHANI A MABASA\nPO BOX 10359\nVORNA VALLEY\n1686\nFNB\nTIRHANI@HOTMAIL.COM\nFirst Notional Bank\n05 NOV 2019\nStatements\n250-655\nFNB Private Clients Cheque Account 62274946163\nSummary in Rand\nZAR\nOpening Balance\nFunds Received (Credits)\nCash Deposits\nOther Deposits\n23,835.56 Dr\nContact us\n54,220.73 Cr\n0.00\n0.00\nThabo Sabeka\ne-Mail\nthabo.sabeka@ fnb.co.za\n(087) 736-6801\nfnb.co.za\nTelephone Number\nWeb\nInter-Account Transfers In\n12,550.00 Cr\n41,670.73 Cr\nE lectronic Payments Received\nDebit Interest Rates (NCA)\nP rime Linked 21.50% - Rebate (If applicable)\nFunds Used (Debits)\nCash Withdrawals (Branch)\nCash Withdrawals (Other)\nCheques Processed (Non Cash)\nDebit Orders/S cheduled Payments\nAccount Payments\nInter-Account Transfers Out\nCard Purchases (S wipes)\nFuel Purchases\n34,200.21 Dr\n0.00\n65\n0\n3\n0\n2,300.00 Dr\n0.00\n1,086.07 Dr\n12,568.00 Dr\n13,175.00 Dr\n5,071.14 Dr\n0.00\n6\n22\n16\n18\n0\nBank Charges\nService Fees\n874.95 Dr\n670.00 Dr\n0.00\nCash Deposit Fees\nCash Handling Fees\nO ther Fees\n0\n0\n0.00\n204.95 Dr\n3\nOther Entries\n0\nInterest on Credit Balance\n0.00\nInterest on Debit Balance\n377.61 Dr\n1\nInward Unpaid Items\nUnpaid Cheques and Debits\nRefunds/Adjustments\n0\n0\n0\n0.00\n0.00\n0.00\nClosing Balance\n5,067.60 Dr\n23,100.00\n1,000.00\nOverdraft Limit\nMonthly Reduction Amount\nPricing Option: Your account is currently on the Bundled pricing option. For more information, please Contact Us or visit our website\nPage 1 of 4\nDelivery Method E1 R04\nEN/20/NV/DDA 30\n1089\nBranch Number\nAccount Number\nDate\nDDA 30/0R/94/KM/KM/PA/P 6/A6/QH/Y\nFNORA\n19/10/19\nFNB Private Clients Cheque Account\n1089\n62274946163\n47827\nCSFZFN0:62274946163\nLON O\nom\nOHO OO\n"
+text = "Telkom\nStatement\nMRS. LISA LUXTON MCINTYRE\n30 DUNSTER AV\nFISH HOEK\nFISH HOEK\nStatement date\n09 Oct 2019\n7975\nAccount no\n337097524\nEFT Ref No\n0033160005030597774\nAccount summary\nReference\nAmount\nDate\nDescription\nBalance brought forward\nR 865.63\n09 Sep 2019\n3602473\nR 1,200.00\nPayment: Thank You\n04 Sep 2019\n6100672\nR 1,000.00\nPayment: Thank You\n04 Oct 2019\nSubtotal\n-R 1,334.37\nInvoice for October\n09 Oct 2019\nA117659851\nR 1,239.46\nSubscription & usage for 0217820738\nSubscription & usage for 0217820738_1\nFixed Voice BB and I\nR 231.69\nFixed Voice BB and I\nR 428.73\nTIAII Access Uncappe\nSubscription & usage for TIN2582148\nTotal due\nR 579.04\n-R 94.91\n-R 94.91\nDue by 31 Oct 2019\nMAKE THE SMART CHOICE AND MOVE TO\nSMARTBROADBAND WIRELESS!\nGet super-fast, reliable, wireless internet with FREE WiFi router.\nTelkom SA SOC Ltd. Reg office: Telkom Park, The Hub, 61 Oak Avenue, Centurion, 0157.Comp Reg No 1991/005476/30.VAT No 4680101146\nDo not detach this portion from this Statement page\nPayment information\nAmount due\n-R 94.91\n00331600050305977740600000000000\nPayment code\n7774\n\u0421ycle\nGroup no\n00331\nControl code\nSystem no\n6000503059\n060\n<<<<< 9 2021 0033 0005 0305 74 >>>>>\nPage 1 of 4\n"
 
 residence = Vision(web_entities, text)
