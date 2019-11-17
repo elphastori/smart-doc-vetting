4215132aacd633846290d930e9a616a352d253f9
Elphas <elphastori@gmail.com>
2019-11-17 06:46:23 +0000

    Updated unit tests for classification

--- keywords/classification.py
+++ keywords/classification.py
@@ -1,20 +1,33 @@
 from confidence import calculate_confidence_id, calculate_confidence_bank, calculate_confidence_employment
 
-def get_classification(words):
+def get_confidences(words):
 
-    id_confidence = calculate_confidence_id(words)
-
-    bank_confidence = calculate_confidence_bank(words)
-
-    employment_confidence = calculate_confidence_employment(words)
-
-    scores = dict(id = id_confidence, bank = bank_confidence, employment = employment_confidence)
-
+    scores = dict(id_result = calculate_confidence_id(words),
+        bank = calculate_confidence_bank(words),
+        employment = calculate_confidence_employment(words))
     valid_scores = dict(filter(lambda x : x[1] != 0, scores.items()))
+    return sorted(valid_scores.items(), key=lambda kv: kv[1], reverse=True)
 
-    sorted_x = sorted(valid_scores.items(), key=lambda kv: kv[1], reverse=True)
 
-    first = sorted_x[0][0]
-    # second = sorted_x[0][0]
+def get_classification(web_words, text_words):
 
-    return first
+    sorted_x = get_confidences(web_words)
+
+    if (len(sorted_x) == 0):
+
+        web_sorted_x = get_confidences(text_words)
+        if (len(web_sorted_x) == 0):
+            return ""
+        return web_sorted_x[0][0]
+
+    xxx = sorted_x[0]
+
+    if (xxx[1] > 1):
+        return xxx[0]
+
+    web_sorted_x = get_confidences(text_words)
+
+    if (len(web_sorted_x) == 0):
+        return xxx[0]
+
+    return web_sorted_x[0][0]
